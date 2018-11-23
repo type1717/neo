20301ed3e6f70c080f0cc970e6c150a5c0ec9840
wooyeon06 <45275750+wooyeon06@users.noreply.github.com>
2018-11-23 14:29:11 +0900

    commit message

--- AdapterPattern/src/Main.java
+++ AdapterPattern/src/Main.java
@@ -4,10 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Adapter adapter = new AdapterImpl();
-		
-		
-		
-		
 		System.out.println(adapter.twiceOf(100f));
 		System.out.println(adapter.halfOf(100f));
 	}
