12f7a0cdfce6c570a9a0c207ff4fa8cca6c3c63e
wooyeon06 <45275750+wooyeon06@users.noreply.github.com>
2018-11-26 13:37:08 +0900

    31 3

--- AdapterPattern/src/Main.java
+++ AdapterPattern/src/Main.java
@@ -9,7 +9,10 @@ public class Main {
 		System.out.println(adapter.twiceOf(100f));	
 		System.out.println(adapter.halfOf(100f));
 		
-		
+		System.out.println(adapter.twiceOf(100f));	
+		System.out.println(adapter.halfOf(100f));
+		System.out.println(adapter.twiceOf(100f));	
+		System.out.println(adapter.halfOf(100f));
 	}
 
 }
