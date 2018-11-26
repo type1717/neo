985b7c2e64bcdbff531af41f7e5433a70272513f
wooyeon06 <45275750+wooyeon06@users.noreply.github.com>
2018-11-26 11:19:09 +0900

    merge

--- AdapterPattern/src/Main.java
+++ AdapterPattern/src/Main.java
@@ -8,9 +8,7 @@ public class Main {
 		Adapter adapter = new AdapterImpl();
 		System.out.println(adapter.twiceOf(100f));
 		System.out.println(adapter.halfOf(100f));
-		System.out.println(adapter.halfOf(100f));
-		System.out.println(adapter.halfOf(100f));
-		System.out.println(adapter.halfOf(100f));
+		System.out.println("merge");
 		System.out.println(adapter.halfOf(100f));
 	}
 
