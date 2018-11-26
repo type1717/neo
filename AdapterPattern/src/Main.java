2930c468b82a7946e5521e40b46e827534a377c6
wooyeon06 <45275750+wooyeon06@users.noreply.github.com>
2018-11-26 11:27:08 +0900

    Update Main.java

--- AdapterPattern/src/Main.java
+++ AdapterPattern/src/Main.java
@@ -7,6 +7,17 @@ public class Main {
 		
 		Adapter adapter = new AdapterImpl();
 		System.out.println(adapter.twiceOf(100f));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		System.out.println(adapter.halfOf(100f));
 	}
 
