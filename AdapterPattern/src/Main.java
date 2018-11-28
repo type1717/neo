22591fb48918301c21dbf54d23cdd5f5dda5b384
wooyeon06 <45275750+wooyeon06@users.noreply.github.com>
2018-11-28 10:04:27 +0900

    sdf

--- AdapterPattern/src/Main.java
+++ AdapterPattern/src/Main.java
@@ -6,16 +6,16 @@ public class Main {
 		
 		
 		Adapter adapter = new AdapterImpl();
-		System.out.println(adapter.twiceOf(100f));
-		System.out.println(adapter.twiceOf(100f));
-		System.out.println(adapter.twiceOf(100f));
-		System.out.println(adapter.twiceOf(100f));
-		System.out.println(adapter.twiceOf(100f));
+		//twiceOf -> harfOf
+		System.out.println(adapter.halfOf(100f));
 		System.out.println(adapter.halfOf(100f));
 		System.out.println(adapter.twiceOf(100f));
 		System.out.println(adapter.halfOf(100f));
 		System.out.println(adapter.twiceOf(100f));
 		System.out.println(adapter.halfOf(100f));
+		System.out.println(adapter.twiceOf(100f));
+		System.out.println(adapter.twiceOf(100f));
+		System.out.println(adapter.twiceOf(100f));
 		System.out.println(adapter.twiceOf(100f));
 		System.out.println(adapter.halfOf(100f));
 	}
