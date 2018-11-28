258a2fb7445a40d9838d0b7db2cf4c648b619038
wooyeon06 <45275750+wooyeon06@users.noreply.github.com>
2018-11-28 09:46:41 +0900

    sdfsd

---
+++ AdapterPattern/src/Main3.java
@@ -0,0 +1,12 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Adapter adapter = new AdapterImpl();
+		System.out.println(adapter.halfOf(100f));
+	}
+
+}
